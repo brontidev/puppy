2f02adaf99cf0341ebee0ed94c33188efa9e21c6
cleo <kai@kaidoesthings.xyz>
2026-04-13 10:44:47 -0400

    accidentally exposed firebase keys and they got revoked immediately

--- src/lib/server/firebase.ts
+++ src/lib/server/firebase.ts
@@ -1,11 +1,14 @@
 import { initializeApp, cert, type App } from "firebase-admin/app"
 import type { ServiceAccount } from "firebase-admin"
-import serviceAccount from "./service-account.json"
+import { env } from "$env/dynamic/private"
 import { getAuth } from "firebase-admin/auth"
 import { getFirestore } from "firebase-admin/firestore"
 import { getApp } from "firebase/app"
 
 let firebaseApp: App;
+
+
+const serviceAccount: ServiceAccount = JSON.parse(atob(env.FIREBASE_SERVICE_ACCOUNT))
 
 try {
     firebaseApp = getApp()
@@ -14,6 +17,8 @@ try {
         credential: cert(serviceAccount as ServiceAccount),
     })
 }
+
+console.log(firebaseApp, serviceAccount)
 
 export const auth = getAuth(firebaseApp)
 export const db = getFirestore(firebaseApp)
